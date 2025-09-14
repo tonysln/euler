346e5d78aefa9bf5ade629c7ce09f8eebe69e369
Anton Slavin <slavinant@gmail.com>
2025-09-14 23:16:15 +0300

    Solve problems 1 and 4

--- euler.py
+++ euler.py
@@ -38,6 +38,40 @@ def collatz_count(n, d, limit=1e10):
 
 
 @timed
+def problem_1(N=1e4):
+    """
+    Multiples of 3 or 5
+    Problem 1
+    """
+    s = 0
+    for i in range(1, int(N)):
+        if i % 3 == 0 or i % 5 == 0:
+            s += i
+
+    return s
+
+
+@timed
+def problem_4(d=3):
+    """
+    Largest Palindrome Product
+    Problem 4
+    """
+    pmax = (-1, -1, -1)
+    for a in range(10**(d-1), 10**d):
+        for b in range(10**(d-1), 10**d):
+            if b <= a:
+                continue
+
+            p = a * b
+            if str(p) == str(p)[::-1]:
+                if p > pmax[0]:
+                    pmax = (p, a, b)
+
+    return pmax
+
+
+@timed
 def problem_14(N=1e6):
     """
     Longest Collatz Sequence
@@ -57,6 +91,10 @@ def problem_14(N=1e6):
 
 @timed
 def problem_28(side):
+    """
+    Number Spiral Diagonals
+    Problem 28
+    """
     n = side*side
     i = round(math.sqrt(n))
     s = 0
@@ -69,7 +107,9 @@ def problem_28(side):
 
 
 if __name__ == '__main__':
-    # print('* Problem 14:\n', problem_14(1e6))
+    print('* Problem 1:\n', problem_1(1e4))
+    print('* Problem 4:\n', problem_4(3))
+    print('* Problem 14:\n', problem_14(1e5))
     print('* Problem 28:\n', problem_28(1001))
 
 
